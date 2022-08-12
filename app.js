2f9958da87f50545ccc6c78e49daa4064900cdbf
AbelTrinidadUNI <demetrio.trinidad2019@fiuni.edu.py>
2022-08-12 03:39:41 -0400

    Se agrego un boton para eliminar una tarea y se creo un nuevo modal para agregar una tarea nueva

--- app.js
+++ app.js
@@ -46,6 +46,17 @@ function buscarTarea(idTarea) {
 }
 
 
+function modalHide() {
+    document.getElementById("modal-backdrop").style.display = "none";
+    document.getElementsByTagName("body")[0].style.overflowY = "visible";
+}
+
+function modalShow() {
+    document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+    document.getElementById("modal-backdrop").style.display = "block";
+
+}
+
 function transferirCard(e, idColDestino) {
     const id = e.dataTransfer.getData("id"); //id de la card
     document.getElementById(idColDestino.replace("contenedor", "columna")).appendChild(document.getElementById(id));
@@ -98,7 +109,25 @@ document.addEventListener("dragstart", (e) => {
 
 window.onload = (e) => {
     init();
+
+    eliminador();
+
 };
+
+function eliminador(){
+    //se eliminara la tarea seleccionada
+    const botones = new Array(...(document.getElementsByClassName("delete-btn")));
+    botones.forEach(i => {
+        i.addEventListener("click", (e) => {
+            const idTarea = parseInt(e.target.id.replace("delete", ""));
+            const pos = buscarTarea(idTarea);
+            tareas.splice(pos, 1);
+            actualizarStorage();
+            console.log(tareas)
+            eliminador();
+        })
+    })
+}
 
 document.getElementById("btn-guardar").addEventListener("click", () => {
     const titulo = document.getElementById("inputTitulo").value;
@@ -107,38 +136,53 @@ document.getElementById("btn-guardar").addEventListener("click", () => {
     const entrega = new Date(document.getElementById("inputEntrega").value);
 
     if (titulo.length > 0 && descripcion.length > 0 && responsable.length > 0 && entrega != "Invalid Date") {
-        tareas.push(new Tarea(titulo, descripcion, responsable, entrega.toDateString()))
+        tareas.push(new Tarea(titulo, descripcion, responsable, entrega.toDateString()));
         actualizarStorage();
-
-
-        let modal = "";
-        const myModalEl = document.getElementById('exampleModal');
-        modal = bootstrap.Modal.getOrCreateInstance(myModalEl);
-        modal.toggle();
-        document.querySelector(".modal-backdrop").style.display = "none"
+        modalHide();
     } else {
         alert("Complete todos los campos antes de guardar la tarea");
     }
 
 });
 
+export function saludar(id) {
+    alert("hola : ", id);
+}
+
+
 
 function toStringTarea(tarea) {
     return `<div class="tarea" id="${tarea.id}" draggable="true">
                 <div class="contenedor">
-                    <div class="titulo-tarea">Titulo: ${tarea.titulo}</div>
-                    <div class="descripcion-tarea">
-                        Descripcion: ${tarea.descripcion}
+                    <div class="card-header">
+                        <h5 class="titulo-tarea">Titulo: ${tarea.titulo}</h5>
+                        <button class="btn btn-danger btn-sm delete-btn btn-close" id="delete${tarea.id}"></button>
                     </div>
-                    <div class="responsable-tarea">
-                        Asignado a: <span class="responsable-nombre">${tarea.responsable}</span>
-                    </div>
-                    <div class="fecha-entrega">
-                        Entrega: ${new Date(tarea.fechaEntrega).toDateString()}
-                    </div>
-                    <div class="estado-tarea" id='estado${tarea.id}'>
-                        Estado: ${tarea.estado}
+
+                    <div class="card-body">
+                        <div class="descripcion-tarea">
+                            Descripcion: ${tarea.descripcion}
+                        </div>
+                        <div class="responsable-tarea">
+                            Asignado a: <span class="responsable-nombre">${tarea.responsable}</span>
+                        </div>
+                        <div class="fecha-entrega">
+                            Entrega: ${new Date(tarea.fechaEntrega).toDateString()}
+                        </div>
+                        <div class="estado-tarea" id='estado${tarea.id}'>
+                            Estado: ${tarea.estado}
+                        </div>
                     </div>
                 </div>
             </div>`
 }
+
+
+
+document.getElementById("modal-show").addEventListener("click", (e) => {
+    modalShow();
+});
+
+document.getElementById("modal-hide").addEventListener("click", (e) => {
+    modalHide();
+});
